98d32a49f1c710de0775049a0dd637910df0ab40
Alejandro Chavez <achavez5@wisc.edu>
2023-12-03 16:44:46 -0600

    init commit
    initialize react app

---
+++ src/App.tsx
@@ -0,0 +1,13 @@
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        Financial roadmap - To be built!
+      </header>
+    </div>
+  );
+}
+
+export default App;
