a0bf4fe08289005de3579b6589c47c4aa424877f
Alejandro Chavez <achavez5@wisc.edu>
2023-12-03 21:01:44 -0600

    complete form validation and submission

--- src/components/AmortizationTable/AmortizationApp.tsx
+++ src/components/AmortizationTable/AmortizationApp.tsx
@@ -1,10 +1,24 @@
-import { useState } from "react";
+import { useState, useEffect } from "react";
 import AmortizationForm from "./AmortizationForm";
 
 const AmortizationApp = () => {
+    const [loanAmount, updateLoanAmount] = useState(100_000);
+    const [termLength, updateTermLength] = useState(360);
+    const [interestRate, updateInterestRate] = useState(5);
+
+    // TODO: remove this useEffect when amort table is complete
+    // console to reduce warning noise
+    useEffect(function () {
+        console.log(loanAmount, termLength, interestRate);
+    }, [loanAmount, termLength, interestRate]);
+
     return (
         <>
-            <AmortizationForm />
+            <AmortizationForm 
+                updateLoanAmount={updateLoanAmount} 
+                updateTermLength={updateTermLength}
+                updateInterestRate={updateInterestRate}
+                />
         </>
     )
 };
--- src/components/AmortizationTable/AmortizationForm.tsx
+++ src/components/AmortizationTable/AmortizationForm.tsx
@@ -1,6 +1,34 @@
-import { Box, Heading, FormLabel, Input, VStack, FormControl, Button} from "@chakra-ui/react";
+import { Box, Heading, FormLabel, Input, VStack, FormControl, Button, FormErrorMessage } from "@chakra-ui/react";
+import { useFormik } from "formik";
+import * as Yup from "yup";
 
-const AmortizationForm = () => {
+// figure out if there is a better way to handle the states
+type AmortizationParameters = {
+    updateLoanAmount: React.Dispatch<React.SetStateAction<number>>,
+    updateTermLength: React.Dispatch<React.SetStateAction<number>>,
+    updateInterestRate: React.Dispatch<React.SetStateAction<number>>
+}
+
+const AmortizationForm = (props: AmortizationParameters) => {
+    const { updateLoanAmount, updateTermLength, updateInterestRate } = props;
+    const formik = useFormik({
+        initialValues: {
+          loanAmount: "", 
+          termLength: "",
+          interestRate: "",
+        },
+        onSubmit: (values) => {
+            updateLoanAmount(parseFloat(values.loanAmount));
+            updateTermLength(parseFloat(values.termLength));
+            updateInterestRate(parseFloat(values.interestRate));
+        },
+        validationSchema: Yup.object({
+          loanAmount: Yup.number().required("Required"),
+          termLength: Yup.number().required("Required"),
+          interestRate: Yup.number().required("Required"),
+        }),
+      });
+
     return (
         <Box>
             <VStack>
@@ -8,44 +36,42 @@ const AmortizationForm = () => {
                     Amortization Table  
                 </Heading>
                 <Box p={6} rounded="md" backgroundColor={"#424242"} /**TODO: adjust the background color to match the theme | TODO: Add drop shadow to give element depth*/>
-                    <VStack spacing={4}>
-                        <FormControl>
-                            <FormLabel htmlFor="loan-amount">Loan amount</FormLabel>
-                            <Input
-                                id="loan-amount"
-                                name="loan-amount"
-                                placeholder="100,000"
-                                type="number"
-                                // TODO: hookup formik stuffs
-                                >
-                            </Input>
-                        </FormControl>
-                        <FormControl>
-                            <FormLabel htmlFor="term-length">Term length (months)</FormLabel>
-                            <Input
-                                id="term-length"
-                                name="term-length"
-                                placeholder="360"
-                                type="number"
-                                // TODO: hookup formik stuffs
-                                >
-                            </Input>
-                        </FormControl>
-                        <FormControl>
-                            <FormLabel htmlFor="interest-rate">Yearly interest rate</FormLabel>
-                            <Input
-                                id="interest-rate"
-                                name="interest-rate"
-                                placeholder="5"
-                                type="number"
-                                // TODO: hookup formik stuffs
-                                >
-                            </Input>
-                        </FormControl>
-                        <Button type="submit" width="full" colorScheme="blue">
-                            Calculate amortization schedule
-                        </Button>
-                    </VStack>
+                    <form onSubmit={formik.handleSubmit}>
+                        <VStack spacing={4}>
+                            <FormControl isInvalid={Boolean(formik.touched.loanAmount && formik.errors.loanAmount)}>
+                                <FormLabel htmlFor="loanAmount">Loan amount</FormLabel>
+                                <Input
+                                    id="loanAmount"
+                                    placeholder="100,000"
+                                    type="number"
+                                    {...formik.getFieldProps("loanAmount")}
+                                />
+                                <FormErrorMessage>{formik.errors.loanAmount}</FormErrorMessage>
+
+                            </FormControl>
+                            <FormControl isInvalid={Boolean(formik.touched.termLength && formik.errors.termLength)}>
+                                <FormLabel htmlFor="termLength">Term length (months)</FormLabel>
+                                <Input
+                                    id="termLength"
+                                    placeholder="360"
+                                    type="number"
+                                    {...formik.getFieldProps("termLength")}
+                                />                                <FormErrorMessage>{formik.errors.termLength}</FormErrorMessage>
+                            </FormControl>
+                            <FormControl isInvalid={Boolean(formik.touched.interestRate && formik.errors.interestRate)}>
+                                <FormLabel htmlFor="interestRate">Yearly interest rate (%)</FormLabel>
+                                <Input
+                                    id="interestRate"
+                                    placeholder="5"
+                                    type="number"
+                                    {...formik.getFieldProps("interestRate")}
+                                />                                <FormErrorMessage>{formik.errors.loanAmount}</FormErrorMessage>
+                            </FormControl>
+                            <Button type="submit" width="full" colorScheme="blue">
+                                Calculate amortization schedule
+                            </Button>
+                        </VStack>
+                    </form>
                 </Box>
             </VStack>
         </Box>
