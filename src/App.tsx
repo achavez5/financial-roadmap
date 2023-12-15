026fa1dff646e8feb82d15015ae1bf1960fd0c73
Alejandro Chavez <achavez5@wisc.edu>
2023-12-14 21:28:17 -0600

    Feat: add placeholder header bar

--- src/App.tsx
+++ src/App.tsx
@@ -6,11 +6,13 @@ import {
 } from "@chakra-ui/react"
 import { ColorModeSwitcher } from "./ColorModeSwitcher"
 import AmortizationApp from "./components/AmortizationTable/AmortizationApp"
+import Header from "./components/Header"
 import theme from "./theme"
 
 export const App = () => (
   <ChakraProvider theme={theme}>
-    <Box textAlign="center" fontSize="xl">
+    <Header />
+    <Box pt="4rem" textAlign="center" fontSize="xl">
       <Grid minH="100vh" p={3}>
         <ColorModeSwitcher justifySelf="flex-end" />
         <VStack spacing={8}>
---
+++ src/components/Header.tsx
@@ -0,0 +1,45 @@
+import { Box, HStack, useColorModeValue, Image, Tooltip } from "@chakra-ui/react";
+import { ArrowForwardIcon } from "@chakra-ui/icons";
+import logo from "../images/placeholderLogo.png"; // TODO: get a logo
+
+const Header = () => {
+
+  return (
+    <Box
+      position="fixed"
+      top={0}
+      left={0}
+      right={0}
+      translateY={0}
+      transitionProperty="transform"
+      transitionDuration=".3s"
+      transitionTimingFunction="ease-in-out"
+      backgroundColor={useColorModeValue("cyan.700", "cyan.900")}
+      zIndex={200} // fixes the header navigation bar to the top of all elements
+    >
+      <Box color="white" maxWidth="1280px" margin="0 auto">
+        <HStack
+          px={16}
+          py={4}
+          justifyContent="space-between"
+          alignItems="center"
+        >
+          <nav>
+            <HStack>
+              {/**  TODO: Work on transition when this gets clicked when sidebar is figured out */}
+              <Tooltip label={`I don't do anything yet`} placement='right-start'>
+                <span><ArrowForwardIcon /></span> 
+              </Tooltip>
+            </HStack>
+          </nav>
+          <Image src={logo} boxSize="1.5em"/>
+          <nav>
+            {/**TODO: figure out what goes here */}
+            <></>
+          </nav>
+        </HStack>
+      </Box>
+    </Box>
+  );
+};
+export default Header;
