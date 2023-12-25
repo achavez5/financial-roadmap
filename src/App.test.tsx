9ff6ea58f87e3592ef5bb2c10c0f531489092df1
Alejandro Chavez <achavez5@wisc.edu>
2023-12-25 14:59:52 -0600

    remove test

--- src/App.test.tsx
+++
@@ -1,9 +0,0 @@
-import React from "react";
-import { screen } from "@testing-library/react";
-import { render } from "./test-utils";
-import { App } from "./App";
-
-test("renders learn react link", () => {
-  render(<App />)
-  // TODO: Actually create a test
-});
