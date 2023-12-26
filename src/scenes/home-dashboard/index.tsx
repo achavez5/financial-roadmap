4dcf0eb13c854a95cfe54bc42a667b1f3652f165
Alejandro Chavez <achavez5@wisc.edu>
2023-12-26 14:02:12 -0600

    add a home page scene

---
+++ src/scenes/home-dashboard/index.tsx
@@ -0,0 +1,12 @@
+import Topbar from "../global/Topbar"
+
+const Home = () => {
+    return (
+        <div>
+            <Topbar />
+            <h1>Home</h1>
+        </div>
+    )
+};
+
+export default Home;
