70eeb9d04f78e2cc3d56d667bed25b1177105b22
Alejandro Chavez <achavez5@wisc.edu>
2024-01-02 21:22:32 -0600

    new string helper

--- src/Libraries/String_Helpers.ts
+++ src/Libraries/String_Helpers.ts
@@ -2,3 +2,7 @@ export let FormatToDollar = new Intl.NumberFormat('en-US', {
     style: 'currency',
     currency: 'USD',
 })
+
+export const CapitalizeFirstCharacterOfWord = (word: string) => {
+    return word.charAt(0).toUpperCase() + word.slice(1)
+}
