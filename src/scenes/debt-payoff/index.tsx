0c0ba7eae8e366272784efd3c8090e3ede7f7277
Alejandro Chavez <achavez5@wisc.edu>
2024-01-02 21:24:45 -0600

    debt payoff placeholder

---
+++ src/scenes/debt-payoff/index.tsx
@@ -0,0 +1,20 @@
+import Topbar from "../global/Topbar";
+import { Box, Typography } from "@mui/material";
+
+
+const DebtPayoff = () => {
+    return (
+        <Box>
+            <Topbar title="Debt Payoff Calculator" />
+            <Box
+                sx={{
+                    margin: "20px",
+                }}
+            >
+                <Typography variant="h2">To be built..</Typography>
+            </Box>
+        </Box>
+    );
+};
+
+export default DebtPayoff;
